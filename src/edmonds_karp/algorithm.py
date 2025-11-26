5b57ff11f8dcf678ec4bb6015e1e44113dad4b37
pixel <edwardoliverthomas@gmail.com>
2025-11-26 14:18:30 +0000

    docs: adding docstrings

--- src/edmonds_karp/algorithm.py
+++ src/edmonds_karp/algorithm.py
@@ -1,23 +1,47 @@
 from collections import deque
 
 class EdmondsKarpGraph:
-    """
-    Docstring for EdmondsKarpGraph
-    """
-    def __init__(self, size):
+    def __init__(self, size: int) -> None:
+        """
+        Initialize an Edmonds-Karp graph with the specified number of vertices.
+        
+        :param size: The number of vertices in the graph
+        """
         self.size = size
         self.adj_matrix = [[0] * size for _ in range(size)]
-        self.vertex_data = [''] * size
+        self.vertex_labels = [''] * size
 
-    def add_edge(self, u, v, capacity):
+    def add_edge(self, u: int, v: int, capacity: int) -> None:
+        """
+        Add a directed edge from vertex u to vertex v with given capacity.
+        
+        :param u: Source vertex index
+        :param v: Destination vertex index
+        :param capacity: Edge capacity (must be positive)
+        """
         if u != v:
             self.adj_matrix[u][v] = capacity
-    
-    def set_vertex_data(self, vertex, data):
+
+    def set_vertex_label(self, vertex: int, label: str) -> None:
+        """
+        Set a label for a vertex (e.g., 'S', 'T', 'U').
+        
+        :param vertex: The vertex index
+        :param label: The label string for the vertex
+        """
         if 0 <= vertex < self.size:
-            self.vertex_data[vertex] = data
-    
-    def bfs(self, s, t, parent):
+            self.vertex_labels[vertex] = label
+
+    def bfs(self, s: int, t: int, parent: list[int]) -> bool:
+        """
+        Find an augmenting path from source s to sink t using BFS.
+        Updates the parent array to track the path found.
+        
+        :param s: Source vertex index
+        :param t: Sink/target vertex index
+        :param parent: List to store the parent of each vertex in the path
+        :return: True if a path exists, False otherwise
+        """
         visited = [False] * self.size
         queue = deque([s])
         visited[s] = True
