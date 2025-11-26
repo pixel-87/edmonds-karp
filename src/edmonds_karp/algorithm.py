0a9a531f2a0ca92d005a49745f521ef1c35e0c85
pixel <edwardoliverthomas@gmail.com>
2025-11-26 13:45:25 +0000

    feat: added bfs search

--- src/edmonds_karp/algorithm.py
+++ src/edmonds_karp/algorithm.py
@@ -17,4 +17,20 @@ class EdmondsKarpGraph:
         if 0 <= vertex < self.size:
             self.vertex_data[vertex] = data
     
-    
+    def bfs(self, s, t, parent):
+        visited = [False] * self.size
+        queue = deque([s])
+        visited[s] = True
+
+        while queue:
+            u = queue.popleft()
+
+            for v in range(self.size):
+                if not visited[v] and self.adj_matrix[u][v] > 0:
+                    queue.append(v)
+                    visited[v] = True
+                    parent[v] = u
+
+                    if v == t:
+                        return True
+        return False
